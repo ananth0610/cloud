f38398ce499c8e9de70cded1bcb3263bc71fd2a0
ananth0610 <apartha85@gmail.com>
2023-03-27 18:31:50 +0000

    first commit

---
+++ cloud.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#Initialize dataframe
+df = pd.DataFrame()
+
+if df.empty:
+  print("DataFrame is empty")
+
+  # initialize list elements
+  data = [10,20,30,40,50,60]
+
+  df = pd.DataFrame(data, columns=['Numbers'])
+  print("populating df now")
+  print(df)
+
+else:
+  print(df)
